7c38bfb10c8fa94852a8f8c54f3eb9ce5a76dc04
griffan <zhangfanqw@gmail.com>
2016-12-05 16:43:36 -0500

    put major coordinates to pc1 pc2

--- ContaminationEstimator.cpp
+++ ContaminationEstimator.cpp
@@ -51,6 +51,7 @@ int ContaminationEstimator::OptimizeLLK()
             std::cout << "Estimation from OptimizeHom:"<<std::endl;
             OptimizeHom(myMinimizer);
         }
+        std::cout << "Alpha:" << (fn.globalAlpha<0.5?fn.globalAlpha:(1-fn.globalAlpha))<<std::endl;
         std::cout << "PC1:" << fn.globalPC[0] << "\tPC2:" << fn.globalPC[1] << std::endl;
     }
     else//contamination source from different population
@@ -85,19 +86,27 @@ int ContaminationEstimator::OptimizeLLK()
                 std::cout << "Estimation from OptimizeHeter:" << std::endl;
                 isHeter = false;
                 OptimizeHom(myMinimizer);
-                std::cerr << "PC1:" << PC[0][0] << "\tPC2:" << PC[0][1] << std::endl;
-                std::cerr << "PC3:" << PC[1][0] << "\tPC4:" << PC[1][1] << std::endl;
-                std::cout << "testAlpha:" << (alpha < 0.5 ? alpha : (1 - alpha)) << std::endl;
+//                std::cerr << "PC1:" << PC[0][0] << "\tPC2:" << PC[0][1] << std::endl;
+//                std::cerr << "PC3:" << PC[1][0] << "\tPC4:" << PC[1][1] << std::endl;
+//                std::cout << "testAlpha:" << (alpha < 0.5 ? alpha : (1 - alpha)) << std::endl;
                 PC[1] = PC[0];
                 isHeter = true;
                 OptimizeHeter(myMinimizer);
   //          }
         }
+        std::cout << "Alpha:" << (fn.globalAlpha<0.5?fn.globalAlpha:(1-fn.globalAlpha))<<std::endl;
+        if(fn.globalAlpha<0.5)
+        {
+            std::cout << "Alpha:" <<fn.globalAlpha<<std::endl;
+        } else
+        {
+            std::cout << "Alpha:" <<1-fn.globalAlpha<<std::endl;
+            std::swap(fn.globalPC,fn.globalPC2);
+        }
         std::cout << "PC1:" << fn.globalPC[0] << "\tPC2:" << fn.globalPC[1] << std::endl;
         std::cout << "PC3:" << fn.globalPC2[0] << "\tPC4:" << fn.globalPC2[1] << std::endl;
     }
 
-    std::cout << "Alpha:" << (fn.globalAlpha<0.5?fn.globalAlpha:(1-fn.globalAlpha))<<std::endl;
     return 0;
 }
 
