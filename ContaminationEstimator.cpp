b452ecae2efa81fa2230bd31f3dc0691607e20d9
griffan <zhangfanqw@gmail.com>
2016-12-05 23:32:01 -0500

    swap PC when necessary

--- ContaminationEstimator.cpp
+++ ContaminationEstimator.cpp
@@ -51,6 +51,7 @@ int ContaminationEstimator::OptimizeLLK()
             std::cout << "Estimation from OptimizeHom:"<<std::endl;
             OptimizeHom(myMinimizer);
         }
+        std::cout << "Alpha:" << (fn.globalAlpha<0.5?fn.globalAlpha:(1-fn.globalAlpha))<<std::endl;
         std::cout << "PC1:" << fn.globalPC[0] << "\tPC2:" << fn.globalPC[1] << std::endl;
     }
     else//contamination source from different population
@@ -93,10 +94,18 @@ int ContaminationEstimator::OptimizeLLK()
                 OptimizeHeter(myMinimizer);
   //          }
         }
+        if(fn.globalAlpha<0.5)
+        {
+            std::cout << "Alpha:" << fn.globalAlpha<<std::endl;
+        }
+        else
+        {
+            std::cout << "Alpha:" << (1-fn.globalAlpha)<<std::endl;
+            std::swap(fn.globalPC,fn.globalPC2);
+        }
         std::cout << "PC1:" << fn.globalPC[0] << "\tPC2:" << fn.globalPC[1] << std::endl;
         std::cout << "PC3:" << fn.globalPC2[0] << "\tPC4:" << fn.globalPC2[1] << std::endl;
     }
-    std::cout << "Alpha:" << (fn.globalAlpha<0.5?fn.globalAlpha:(1-fn.globalAlpha))<<std::endl;
 
 
     return 0;
